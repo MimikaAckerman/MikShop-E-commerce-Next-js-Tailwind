9bbfe29c14479ec166aa09f8f9666a099ed8979b
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-03-31 13:37:44 +0200

    holis

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,7 +1,15 @@
-import Layout from '../../components/Layout'
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+import { useEffect, useReducer } from "react";
+import axios from "axios";
 
-function OrderScreen(){
-return <Layout title={`Order ${orderId}`}></Layout>
+function reducer(state,action){
+    switch(action.type){
+        case 'FETCH_REQUEST':
+            return {...state,loading:true,error:''};
+            case 'FETCH_FAIL':
+                return {...state,loading:false,error:action.payload};
+                default:
+                    state;
+    }
 }
-OrderScreen.auth = true;
-export default OrderScreen;
