26ccae58de826f49c2ccaa9e38658aee9a109bf6
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-03-31 17:40:08 +0200

    holist

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -12,6 +12,8 @@ function reducer(state, action) {
   switch (action.type) {
     case "FETCH_REQUEST":
       return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, order: action.payload, error: "" };
     case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
     default:
@@ -48,7 +50,7 @@ function OrderScreen() {
     paymentMethod,
     orderItems,
     itemsPrice,
-   
+
     isPaid,
     paidAt,
     isDelivered,
