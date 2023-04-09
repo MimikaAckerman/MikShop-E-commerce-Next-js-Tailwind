c4c75d3c77975d4663358eee3476cd5e22e3e4bc
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-04-09 21:51:41 +0200

    holis

--- components/Layout.js
+++ components/Layout.js
@@ -75,19 +75,8 @@ signOut({callbackUrl:'/login'})
                   >
                     {session.user.name}
                     <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
-                      <Menu.Item>
-                        <DropdownLink className="dropdown-link" href="/profile">
-                          Profile
-                        </DropdownLink>
-                      </Menu.Item>
-                      <Menu.Item>
-                        <DropdownLink
-                          className="dropdown-link"
-                          href="/order-history"
-                        >
-                          Order History
-                        </DropdownLink>
-                      </Menu.Item>
+                  
+                     
                       {session.user.isAdmin && (
                         <Menu.Item>
                           <DropdownLink
